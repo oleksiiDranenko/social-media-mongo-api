2d576a2ed6f96d67e1ecf91caaa117d831f706f4
oleksiiDranenko <alexdranenko2004@icloud.com>
2023-08-07 14:45:16 +0100

    added basic register functionality

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,28 @@
 // libraries
 import express from 'express'
+import bcrypt from 'bcrypt'
+// model
+import { UserModel } from '../models/User.js'
 
 export const userRouter = express.Router()
+
+
+userRouter.post('/register', async (req, res) => { 
+    const { username, password } = req.body;
+
+    const user = await UserModel.findOne({ username })
+
+    if (user) {
+        res.json({
+            error: "User already exists"
+        })
+    } else {
+        const hashedPassword = await bcrypt.hash(password, 5)
+
+        const newUser = new UserModel({ username, password: hashedPassword })
+
+        await newUser.save()
+
+        res.json(newUser) 
+    }
+})
