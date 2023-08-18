d4a4fffc4f0282251d375ec53d83da2bee7a1cc3
oleksiiDranenko <alexdranenko2004@icloud.com>
2023-08-18 23:12:02 +0100

    added functionality to display the likes count

--- routes/likes.js
+++ routes/likes.js
@@ -5,11 +5,34 @@ import { LikesModel } from '../models/Like.js';
 
 export const likesRouter = express.Router()
 
+
+// GET
+
+likesRouter.get('/post-likes-count/:postId', async (req, res) => {
+    const { postId } = req.params;
+
+    try {
+        const likesCount = await LikesModel.countDocuments({ postId });
+
+        res.json({
+            count: likesCount
+        })
+    }
+    catch {
+        res.json({
+            error: "Unable to get the likes count"
+        })
+    }
+})
+
+
+// POST
+
 likesRouter.post('/like', async (req, res) => {
     const { userId, postId } = req.body;
 
     try {
-        const like = await LikesModel.findOne({ userId: userId, postId: postId });
+        const like = await LikesModel.findOne({ userId, postId });
 
         if (like) {
             await LikesModel.findByIdAndDelete(like._id);
