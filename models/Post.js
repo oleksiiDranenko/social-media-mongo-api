dc43d719ff571c5fb8d58e246ea64eac49ed30f6
oleksiiDranenko <alexdranenko2004@icloud.com>
2024-02-09 00:10:25 +0000

    added userAvatar to the post schema

--- models/Post.js
+++ models/Post.js
@@ -2,6 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const PostSchema = new Schema({
     	username: { type: String, required: true },
+		userAvatar: { type: Number, required: true },
     	userId: { type: String, required: true },
     	value: { type: String, required: true },
     	img: { type: String, required: false },
