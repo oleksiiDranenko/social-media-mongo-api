85fadaacd92f2955bae4309a17330bbc40208384
oleksiiDranenko <alexdranenko2004@icloud.com>
2024-09-15 21:04:36 +0100

    fixed date

--- routes/posts.js
+++ routes/posts.js
@@ -59,7 +59,7 @@ postsRouter.post('/add', async (req, res) => {
     try {
         const currentDate = new Date();
         const options = { 
-            weekday: 'short', 
+            month: 'short', 
             day: 'numeric', 
             hour: '2-digit', 
             minute: '2-digit', 
