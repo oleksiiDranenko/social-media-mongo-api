8ae85c9c00b377e869a0e42cdc928d2a2283624d
oleksiiDranenko <alexdranenko2004@icloud.com>
2024-09-26 12:31:48 +0100

    fix

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ mongoose.connect(dbConnectionString, {
 })
 
 
-app.listen(3001, () => {
-  console.log('Server is running');
-})
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`Server is running on port ${process.env.PORT || 3000}`);
+});
