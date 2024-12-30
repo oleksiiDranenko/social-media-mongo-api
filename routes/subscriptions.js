a6f08b3c6af095e505308862888845b2eee27548
oleksiiDranenko <alexdranenko2004@icloud.com>
2024-12-30 17:49:22 +0000

    unsubscribe request added

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -1,7 +1,9 @@
 // libraries
 import express from 'express';
+import mongoose from 'mongoose';
 // model
 import { SubscriptionModel } from '../models/Subscription.js';
+import { UserModel } from '../models/User.js';
 
 export const subscriptionsRouter = express.Router()
 
@@ -35,13 +37,101 @@ subscriptionsRouter.get('/is-subscribed', async (req, res) => {
     }
 });
 
+//
+
+subscriptionsRouter.get('/get-subscriptions-to/:subscriberId', async (req, res) => {
+    const { subscriberId } = req.params;
+
+    try {
+        const subscriptions = await SubscriptionModel.find({ subscriber: subscriberId });
+
+        const subscribedToIds = subscriptions
+            .map(sub => sub.subscribedTo)
+            .filter(mongoose.Types.ObjectId.isValid); 
+
+
+        const users = await UserModel.find({ _id: { $in: subscribedToIds } });
+
+        res.json(users); 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+subscriptionsRouter.get('/get-subscriptions-to-num/:subscriberId', async (req, res) => {
+    const { subscriberId } = req.params;
+
+    try {
+        const subscriptionsNum = await SubscriptionModel.countDocuments({ subscriber: subscriberId });
+
+        res.json({
+            subNum: subscriptionsNum 
+        });
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: "Unable to fetch subscription count" });
+    }
+});
+
+
+//
+
+
+subscriptionsRouter.get('/get-subscribers/:subscribedToId', async (req, res) => {
+    const { subscribedToId } = req.params;
+
+    try {
+        const subscriptions = await SubscriptionModel.find({ subscribedTo: subscribedToId });
+
+        const subscriberIds = subscriptions
+            .map(sub => sub.subscriber)
+            .filter(mongoose.Types.ObjectId.isValid); 
+
+
+        const users = await UserModel.find({ _id: { $in: subscriberIds } });
+
+        res.json(users); 
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+subscriptionsRouter.get('/get-subscribers-num/:subscribedToId', async(req, res) => {
+    const { subscribedToId } = req.params;
+
+    try {
+        const subscriptionsNum = await SubscriptionModel.countDocuments({ subscribedTo: subscribedToId });
+
+        res.json({
+            subNum: subscriptionsNum 
+        });
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: "Unable to fetch subscription count" });
+    }
+})
+
+
+
 
 // POST
 
 subscriptionsRouter.post('/subscribe', async (req, res) => {
-    const { subscriberId, subscribedToId } = req.body;
+    const { subscriberId, subscribedToId } = req.query;
 
     try {
+        const subscription = await SubscriptionModel.findOne({ subscriber: subscriberId, subscribedTo: subscribedToId })
+
+        if(subscription){
+            res.json({
+                message: "Subscription already exists"
+            })
+            return
+        }
         const newSubscription = new SubscriptionModel({
             subscriber: subscriberId,
             subscribedTo: subscribedToId
@@ -58,3 +148,28 @@ subscriptionsRouter.post('/subscribe', async (req, res) => {
     }
 });
 
+// DELETE
+
+subscriptionsRouter.delete('/unsubscribe/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await SubscriptionModel.findByIdAndDelete(id)
+        
+        if (result) {
+            res.status(200).json({
+                message: "Unsubscribed succesfully"
+            })
+        } else {
+            res.status(500).json({
+                message: "Unable to unsubscribe"
+            })
+        }
+        
+    } catch (error) {
+        res.status(500).json({
+            error: "Unable to unsubscribe",
+            message: error.message
+        });
+    }
+})
